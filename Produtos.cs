aba54d6d3fffbc54d1c8fcd0df75247c6f363320
Pedro Emilio Martinelli <pedroemiliomartinelli@protonmail.com>
2023-10-16 16:32:37 -0300

    COMITADO123

---
+++ Produtos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosProduto
+{
+    internal class Produtos
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " Unidades , total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produtos p = new Produtos();
+
+            Console.WriteLine("ENTRE OS DADOS DO PRODUTO");
+
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("preco: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Qual a quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Dados do produto: " + p);
+
+
+
+        }
+    }
+}
